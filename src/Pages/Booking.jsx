aaad493c4b1d0bd5f51ce65e8ffc6a09ef274dee
Gabe Kouki <gabekouki02@gmail.com>
2025-01-11 01:13:51 -0500

    oopsies

--- src/Pages/Booking.jsx
+++ src/Pages/Booking.jsx
@@ -1,44 +1,58 @@
-import React from 'react';
-import { supabase } from '../Utils/SupabaseClient';
-import './Booking.css';
+import React from 'react'
 
 const Booking = () => {
-  const [data, setData] = React.useState([]);
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
 
-  const fetchArtists = async () => {
-    const { data, error } = await supabase
-      .from("users")
-      .select("*")
+export default Booking
 
-      setData(data);
-    if (error) {
-      throw error;
-    }
-    console.log("Artists:", data);
-  };
+
+
+// import React from 'react';
+// import { supabase } from '../Utils/SupabaseClient';
+// import './Booking.css';
+
+// const Booking = () => {
+//   const [data, setData] = React.useState([]);
+
+//   const fetchArtists = async () => {
+//     const { data, error } = await supabase
+//       .from("users")
+//       .select("*")
+
+//       setData(data);
+//     if (error) {
+//       throw error;
+//     }
+//     console.log("Artists:", data);
+//   };
   
 
 
 
 
-  return (
-    <div className="BookingContainer">
-      <h1>Hello</h1>
-      <button onClick={fetchArtists}>Fetch Artists</button>
-      <div className="artist-selection">
-        <h2>Select an Artist</h2>
-        <div className="artist-grid">
-          {data.map((artist) => (
-            <div className="artist-card" key={artist.id}>
-              <h3>{artist.name}</h3>
-              <p>{artist.email}</p>
-              <p>{artist.phone}</p>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="BookingContainer">
+//       <h1>Hello</h1>
+//       <button onClick={fetchArtists}>Fetch Artists</button>
+//       <div className="artist-selection">
+//         <h2>Select an Artist</h2>
+//         <div className="artist-grid">
+//           {data.map((artist) => (
+//             <div className="artist-card" key={artist.id}>
+//               <h3>{artist.name}</h3>
+//               <p>{artist.email}</p>
+//               <p>{artist.phone}</p>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default Booking;
+// export default Booking;
